b957d6dd71c35c71ed94cbc93c8db002329d25d3
Logan Fleur <effleurager@gmail.com>
2016-08-03 17:18:12 +0200

    Fixed app.js config

--- Go Ride/app.js
+++ Go Ride/app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-var sql = require('mssql');
+var sql = require('node-mssql');
 
 var app = express();
 
