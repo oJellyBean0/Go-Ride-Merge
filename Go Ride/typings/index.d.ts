597746b6d981c2a9aa62731cc3e41adbd78f3b3f
Logan Fleur <effleurager@gmail.com>
2016-08-03 17:37:16 +0200

    Added Typings

--- Go Ride/typings/index.d.ts
+++ Go Ride/typings/index.d.ts
@@ -2,5 +2,7 @@
 /// <reference path="globals/debug/index.d.ts" />
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/inherits/index.d.ts" />
+/// <reference path="globals/jade/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/stylus/index.d.ts" />
+/// <reference path="modules/mssql/index.d.ts" />
