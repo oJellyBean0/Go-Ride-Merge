b5ab5bfcdbd64dee8ce6e4907159f92ff6224e23
Agnes Kijewski <s213329913@nmmu.ac.za>
2016-08-27 22:36:34 +0200

    login page now calls backend directly. Post method needs to be confirmed

--- Go Ride/public/javascripts/login.js
+++
@@ -1,6 +0,0 @@
-login = function(){
-    var usernameInput = document.getElementById("username").value;
-    var passwordInput = document.getElementById("password").value;
-    console.log(usernameInput+" " + passwordInput);
-
-}
