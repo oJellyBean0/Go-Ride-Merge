9be8069a2e6b99d553cfa703f3d6d1f902446f96
Agnes Kijewski <s213329913@nmmu.ac.za>
2016-08-27 22:14:17 +0200

    login.js now recieves username and password entered

---
+++ Go Ride/public/javascripts/login.js
@@ -0,0 +1,6 @@
+login = function(){
+    var usernameInput = document.getElementById("username").value;
+    var passwordInput = document.getElementById("password").value;
+    console.log(usernameInput+" " + passwordInput);
+
+}
