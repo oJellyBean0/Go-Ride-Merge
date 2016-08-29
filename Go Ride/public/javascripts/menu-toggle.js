c35805b96212c7aa68e3fb2faded4ed65b4950e0
Logan Fleur <effleurager@gmail.com>
2016-08-29 14:44:23 +0200

    Automated Toggle on Button loss

--- Go Ride/public/javascripts/menu-toggle.js
+++ Go Ride/public/javascripts/menu-toggle.js
@@ -2,3 +2,11 @@ $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+$(window).resize(function () {
+    var wrapper = $("#wrapper");
+    var toggling = wrapper.hasClass("toggled");
+    if (toggling) {
+        $("#wrapper").toggleClass("toggled");
+    }
+});
