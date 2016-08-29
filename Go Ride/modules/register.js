4b4bf4dcb7336f050e245213fffc5b9f72a6253e
Logan Fleur <effleurager@gmail.com>
2016-08-30 00:34:50 +0200

    Fixed Registration

--- Go Ride/modules/register.js
+++ Go Ride/modules/register.js
@@ -1,9 +1,25 @@
 var sql = require('node-mssql');
+var mssql = require('mssql');
 var config = {
     username: 'JN08User',
     password: '2yWkBhRQ',
     host: 'openbox.nmmu.ac.za\\wrr',
     db: 'JN08'
+};
+var dbConfig = {
+    server: config.host,
+    port: config.port || 1433,
+    user: config.username,
+    password: config.password,
+    database: config.db,
+    options: {
+        encrypt: config.encryption || false,
+        pool: {
+            max: 100,
+            min: 0,
+            idleTimeoutMillis: 30000
+        }
+    }
 };
 
 
@@ -14,48 +30,108 @@ exports.tryRegister = function (IDnumber, name, surname, username, password, pic
         console.log(sql);
         callback(false, error);
     };
+
+    var IDTest = function (results) {
+        if (results.length > 0) {
+            err = "ID Number is already registered";
+            callback(false, err);
+            return;
+        }
+        query.where({
+            'Username': username
+        });
+        query.select(UserTest, errorHandler);
+    };
+
+    var UserTest = function (results) {
+        if (results.length > 0) {
+            err = "Username is taken";
+            callback(false, err);
+            return;
+        }
+        UserInsert();
+    };
+
+    var UserInsert = function () {
+        var params = {
+            'IDNumber': IDnumber,
+            'Name': name,
+            'Surname': surname,
+            'Username': username,
+            'Password': password,
+            'UserType': "Passenger",
+            'Blocked': "0"
+        };
+        var tableName = '[dbo].[user]';
+        var connObj = mssql.connect(dbConfig, function (err) {
+            if (err) {
+                errorHandler(err, '');
+            }
+            else {
+                var sql = 'INSERT INTO ' + tableName;
+                sql += ' (' + Object.keys(params).join(',') + ') VALUES (';
+                Object.keys(params).forEach(function (key) {
+                    sql += '\'' + params[key] + '\', ';
+                });
+                sql = sql.slice(0, -2);
+                sql += ')';
+                var request = new mssql.Request(connObj);
+                request.query(sql, function (err, recordset) {
+                    connObj.close();
+                    if (err) {
+                        errorHandler(err, sql);
+                    }
+                    else {
+                        console.log(recordset);
+                        LocationInsert();
+                    }
+                });
+            }
+        });
+    };
+
+    var LocationInsert = function () {
+        var params = {
+            'IDNumber': IDnumber,
+            'StreetNumber': streetNumber,
+            'StreetName': streetName,
+            'Suburb': suburb,
+            'Town': town,
+            'Province': province
+        };
+        var tableName = '[dbo].[Location]';
+        var connObj = mssql.connect(dbConfig, function (err) {
+            if (err) {
+                errorHandler(err, '');
+            }
+            else {
+                var sql = 'INSERT INTO ' + tableName;
+                sql += ' (' + Object.keys(params).join(',') + ') VALUES (';
+                Object.keys(params).forEach(function (key) {
+                    sql += '\'' + params[key] + '\', ';
+                });
+                sql = sql.slice(0, -2);
+                sql += ')';
+                var request = new mssql.Request(connObj);
+                request.query(sql, function (err, recordset) {
+                    connObj.close();
+                    if (err) {
+                        errorHandler(err, sql);
+                    }
+                    else {
+                        console.log(recordset);
+                        callback(true);
+                        return;
+                    }
+                });
+            }
+        });
+    };
+
     var query = new sql.Query(config);
     query.table('[dbo].[user]');
     query.where({
         'IDNumber': IDnumber
     });
-    query.select(function (results) {
-        if (results.length > 0) {
-            err = "ID Number is already registered";
-            callback(false, err);
-        }
-        query.where({
-            'Username': username
-        });
-        query.select(function (results) {
-            if (results.length > 0) {
-                err = "Username is taken";
-            }
-            query.data({
-                'IDNumber': IDnumber,
-                'Name': name,
-                'Surname': surname,
-                'Username': username,
-                'Password': password,
-                'UserType': "Passenger",
-                'Blocked': "0"
-            });
-            query.insert(function (results) {
-                console.log(results);
-                query.table('[dbo].[Location]');
-                query.data({
-                    'IDNumber': IDnumber,
-                    'StreetNumber': streetNumber,
-                    'StreetName': streetName,
-                    'Suburb': suburb,
-                    'Town': town,
-                    'Province': province
-                });
-                query.insert(function (results) {
-                    console.log(results);
-                    callback(true);
-                });
-            }, errorHandler);
-        }, errorHandler);
-    }, errorHandler);
+    query.select(IDTest, errorHandler);
 };
