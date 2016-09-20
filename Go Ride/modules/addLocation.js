9028d83f3fa0236447586480daa13fca5936d6b1
Logan Fleur <effleurager@gmail.com>
2016-09-20 21:34:51 +0200

    Added untested addLocation.js

---
+++ Go Ride/modules/addLocation.js
@@ -0,0 +1,51 @@
+var mssql = require('mssql');
+var dbConfig = {
+    server: 'openbox.nmmu.ac.za\\wrr',
+    port: 1433,
+    user: 'JN08User',
+    password: '2yWkBhRQ',
+    database: 'JN08',
+    options: {
+        encrypt: false,
+        pool: {
+            max: 100,
+            min: 0,
+            idleTimeoutMillis: 30000
+        }
+    }
+};
+var connectionError = 'Unable to Connect to Server';
+
+exports.tryAddEvent = function (username, callback) {
+    var errorHandler = function (error, sql) {
+        console.log(error);
+        console.log(sql);
+        connObj.close();
+        callback(false, error);
+    };
+
+    var getID = function () {
+        var tableName = '[JN08].[dbo].[User]';
+        var sql = 'SELECT UserID FROM ' + tableName;
+        var request = new mssql.Request(connObj);
+        request.input("Username", mssql.NVarChar, username);
+        sql += " WHERE Username=@Username";
+        request.query(sql, function (err, recordset) {
+            if (err) errorHandler(err, sql);
+            else getCategory(recordset[0].UserID);
+        });
+    };
+
+    var testLocation = function (userID) {
+        var tableName = '[JN08].[dbo].[Location]';
+        var sql = 'SELECT * FROM ' + tableName;
+        var request = new mssql.Request(connObj);
+        request.input("UserID", mssql.NVarChar, userID);
+        sql += " WHERE UserID=@UserID";
+    };
+
+    var connObj = mssql.connect(dbConfig, function (err) {
+        if (err) errorHandler(err, connectionError);
+        else getID();
+    });
+};
