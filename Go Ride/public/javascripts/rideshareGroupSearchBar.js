353f4e802f93458b94ef92c4c7526b0da5be5308
Agnes Kijewski <s213329913@nmmu.ac.za>
2016-09-20 15:57:40 +0200

    Shell for viewRideshareGroups created

---
+++ Go Ride/public/javascripts/rideshareGroupSearchBar.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    $("#searchBar").on("input", function () {
+        var savedSearchTerm = $("#searchBar").val();
+        $.post("/event", { searchTerm: savedSearchTerm }, function (data) {
+            if (savedSearchTerm == $("#searchBar").val()) {
+                $("#eventList").empty();
+                $.each(data.events, createItem);
+            }
+        });
+    });
+
+    $.post("/event", { searchTerm: "" }, function (data) {
+        console.log("posting");
+        $.each(data.events, createItem);
+    });
+});
+
+var createItem = function (key, val) {
+    $("<a/>", {
+        "class": "list-group-item",
+        "href": "editEvent?eventName=" + val.EventName,
+        html: val.EventName
+    }).appendTo("#eventList");
+};
