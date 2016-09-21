b1013c7768b118407cf818adbe49d1eafee31b23
Agnes Kijewski <s213329913@nmmu.ac.za>
2016-09-21 19:56:43 +0200

    Rough outline for add rideshare Group

--- Go Ride/public/javascripts/rideshareGroupSearchBar.js
+++ Go Ride/public/javascripts/rideshareGroupSearchBar.js
@@ -1,39 +1,15 @@
 $(document).ready(function () {
 
-    $("#searchBar").on("input", function () {
-        var savedSearchTerm = $("#searchBar").val();
-        $.post("/event", { searchTerm: savedSearchTerm }, function (data) {
-            if (savedSearchTerm == $("#searchBar").val()) {
-                $(".eventList").empty();
-                $.each(data.events, createItem);
-            }
-        });
-    });
-
     $.post("/event", { searchTerm: "" }, function (data) {
         console.log("posting");
-        $.each(data.events, createItem);
+        console.log(data.events);
+        $.each(data.events,function (key, val) {
+            $("<option/>", {
+                
+                "class": "my-new-list",
+                html: val.EventName
+            }).appendTo("#dropdownMenu");
     });
-    $.post("/event", { searchTerm: "" }, function (data) {
-        console.log("posting");
-        $.each(data.events, searchEvent);
-    });
+    }) 
 });
-
-var createItem = function (key, val) {
-    $("<option/>", {
-        "class": "list-group-item",
-        html: val.EventName
-    }).appendTo(".eventList");
-};
-
-
-var searchEvent = function(key,val){
-        $('#item-type').selectize()};
-var eventItem = function (key, val) {
-    $("<option/>", {
-        "class": "list-group-item",
-        html: val.EventName
-    }).appendTo(".eventList");
-};
 
