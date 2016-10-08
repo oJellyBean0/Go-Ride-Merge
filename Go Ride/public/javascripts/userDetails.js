b6dc461c2eb5c1865d41737e5b3ffade3ae453e4
Richard Smart <richardosmart@gmail.com>
2016-10-08 18:37:57 +0200

    asdf

--- Go Ride/public/javascripts/userDetails.js
+++ Go Ride/public/javascripts/userDetails.js
@@ -3,23 +3,35 @@ var Username
 var Name
 var Surname
 
-$.getJSON('/getUser', {}, function (data) {
-  JSON = data
 
-  $('#usernameHeading').text(JSON.users.Username)
-  $('#name').text(JSON.users.Name)
-  $('#nameInput').text(JSON.users.Name)
-  $('#surname').text(JSON.users.Surname)
-  $('#surnameInput').text(JSON.users.Surname)
-  $('#username').text(JSON.users.Username)
-  $('#usernameInput').text(JSON.users.Username)
-  $('#password').text(JSON.users.Password)
-  $('#passwordInput').text(JSON.users.Password)
+
+$(document).ready(function () {
+
+$.getJSON('/getUser', {}, function (data) {
+  JSON = data;
+  console.log("testTEXT");
+  console.log(data.users.Name);
+  $('#usernameHeading').text(JSON.users[0].Username);
+  $('#name').text(JSON.users[0].Name);
+  $('#nameInput').text(JSON.users[0].Name);
+  $('#surname').text(JSON.users[0].Surname);
+  $('#surnameInput').text(JSON.users[0].Surname);
+  $('#username').text(JSON.users[0].Username);
+  $('#usernameInput').text(JSON.users[0].Username);
+  $('#password').text(JSON.users[0].Password);
+  $('#passwordInput').text(JSON.users[0].Password);
 })
 
 $('#submitUserDetails').click(function () {
-  $.post("/editUser", , {}, String)    //need logan to verify, dont want to delete my only test entry XD
-  $('#' + AreaID1).remove()
-  console.log(AreaID1)
+  $.post("/editUser");
+  $('#' + AreaID1).remove();
+  console.log(AreaID1);
+})
+
+});
+
+('#buttonEdit').click(function () {
+    console.output("asdfasdfasdf")
+    $('#nameInput').text(JSON.users[0].Name);
 })
 
