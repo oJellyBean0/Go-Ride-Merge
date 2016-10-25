3fcdb51a9f2d4b845e039cafb47fc2aa187ee67c
Richard Smart <richardosmart@gmail.com>
2016-10-25 19:14:51 +0200

    modified manageLocations

--- Go Ride/public/javascripts/manageLocations.js
+++ Go Ride/public/javascripts/manageLocations.js
@@ -72,7 +72,26 @@ $('#acceptDeleteLocation').click(function () {
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
+var placeSearch, autocomplete;
+      var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+      };
+
 function initAutocomplete () {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+  /** @type {!HTMLInputElement} */(document.getElementById('pac-input')),
+  {types: ['geocode']});
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -33.963373, lng: 25.616230},
     zoom: 13,
@@ -85,6 +104,26 @@ function initAutocomplete () {
     overviewMapControl: true,
     rotateControl: true
   })
+
+function fillInAddress() {
+      // Get the place details from the autocomplete object.
+      var place = autocomplete.getPlace();
+
+      for (var component in componentForm) {
+        document.getElementById(component).value = '';
+        document.getElementById(component).disabled = false;
+      }
+
+      // Get each component of the address from the place details
+      // and fill the corresponding field on the form.
+      for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+        if (componentForm[addressType]) {
+          var val = place.address_components[i][componentForm[addressType]];
+          document.getElementById(addressType).value = val;
+        }
+      }
+    }
 
   
   // Create the search box and link it to the UI element.
