3a274db93cf204f701dea4a62b31c40615aa7922
Agnes Kijewski <s213329913@nmmu.ac.za>
2016-10-27 20:00:51 +0200

    Search list responds to searches through own and all rideshare groups

--- Go Ride/public/javascripts/rideshareGroupSearchBar.js
+++ Go Ride/public/javascripts/rideshareGroupSearchBar.js
@@ -1,25 +1,73 @@
 $(document).ready(function () {
 
+    $("#filterRideshareGroups").click(function (e) {
+        var filterType = $("#filterRideshareGroups").attr("data-filterType");
+        console.log($("#filterRideshareGroups").attr("data-filterType"));
+        if (filterType == "all") {
+            $("#filterRideshareGroups span").text('Filter All');
+            $("#filterRideshareGroups").attr("data-filterType", "own");
+            console.log($("#filterRideshareGroups").attr("data-filterType"));
+            $("#filterRideshareGroups").css({ position: "relative", left: "-10" });
+            var savedSearchTerm = $("#searchBar").val();
+            $.post("/searchParticipatingRideshares", { searchTerm: savedSearchTerm }, function (data) {
+
+                if (savedSearchTerm == $("#searchBar").val()) {
+                    $("#eventList").empty();
+                    $.each(data.rideshares, createItem);
+                }
+            });
+        }
+        else if (filterType == "own") {
+            $("#filterRideshareGroups span").text("Filter Own List")
+            $("#filterRideshareGroups").attr("data-filterType", "all");
+            console.log($("#filterRideshareGroups").attr("data-filterType"));
+            $("#filterRideshareGroups").css({ position: "relative", left: "-50" });
+            var savedSearchTerm = $("#searchBar").val();
+            $.post("/searchRideshares", { searchTerm: savedSearchTerm }, function (data) {
+
+                if (savedSearchTerm == $("#searchBar").val()) {
+                    $("#eventList").empty();
+                    $.each(data.rideshares, createItem);
+                }
+            });
+        } else {
+            console.log("You fucked up");
+        }
+    })
+
     $("#searchBar").on("input", function () {
         var savedSearchTerm = $("#searchBar").val();
-        $.post("/searchParticipatingRideshares", { searchTerm: savedSearchTerm }, function (data) {
-            
-            if (savedSearchTerm == $("#searchBar").val()) {
-                $("#eventList").empty();
-                $.each(data.rideshares, createItem);
-            }
-        });
+        var filterType = $("#filterRideshareGroups").attr("data-filterType");
+        console.log(filterType);
+        if (filterType == "own") {
+            $.post("/searchParticipatingRideshares", { searchTerm: savedSearchTerm }, function (data) {
+
+                if (savedSearchTerm == $("#searchBar").val()) {
+                    $("#eventList").empty();
+                    $.each(data.rideshares, createItem);
+                }
+            });
+        }
+        else {
+            $.post("/searchRideshares", { searchTerm: savedSearchTerm }, function (data) {
+
+                if (savedSearchTerm == $("#searchBar").val()) {
+                    $("#eventList").empty();
+                    $.each(data.rideshares, createItem);
+                }
+            });
+        }
     });
 
     $.post("/searchParticipatingRideshares", { searchTerm: "" }, function (data) {
-        
+
         console.log("posting");
         console.log(data);
         $.each(data.rideshares, createItem);
     });
-    
+
     $('#viewRideshareDetails').hide();
-    
+
 });
 
 var createItem = function (key, val) {
@@ -35,7 +83,7 @@ var createItem = function (key, val) {
         $('#carIcon').hide();
         $('#viewRideshareDetails').show();
         console.log(e.target.getAttribute("data-RideshareNo"));
-        $.post( '/getRideshare',{ rideshareNo: RideshareNo}, function( data ) {
+        $.post('/getRideshare', { rideshareNo: RideshareNo }, function (data) {
             console.log(data);
             var destination = data.rideshares[0].Destination;
             var driver = data.rideshares[0].Driver[0].Driver;
@@ -46,23 +94,23 @@ var createItem = function (key, val) {
                     html: val.Passenger
                 });
                 item1.appendTo('#passengerList');
-                
+
             });
-            if(data.rideshares[0].Passengers.length == 0){
+            if (data.rideshares[0].Passengers.length == 0) {
                 $('#passengerList').text('No Passengers');
             };
-            
+
             var price = data.rideshares[0].Price;
-            
+
             $('#rideshareNo').val(rideshareNo);
             $('#destination').text(destination);
             $('#driver').text(driver);
             $('#pricekm').text(price);
             $('#pricekmDialog').val(price);
-            
+
             var isDriver = data.rideshares[0].isDriver;
             console.log(isDriver);
-            if(!isDriver)
+            if (!isDriver)
                 $("#changePetrolCost").hide();
             else
                 $("#requestEditRoute").hide();
