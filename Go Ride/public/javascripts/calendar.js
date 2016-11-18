c48139d6fd7bf5078f0b9fc530bc6d6d7a4f4d28
Agnes Kijewski <s213329913@nmmu.ac.za>
2016-11-18 22:34:57 +0200

    changed label to div and added styling

--- Go Ride/public/javascripts/calendar.js
+++ Go Ride/public/javascripts/calendar.js
@@ -12,20 +12,22 @@ var createItem = function (key, val) {
     var timeArr = arr[1].split(':');
     var time = timeArr[0]+':'+timeArr[1];
     var item = $("<div/>", {
-        "class": "list-group-item",
+        "class": "list-group-item; changeElement",
 
         html: 
         "<div class='row'>"+
             "<div class='col-md-4'>"+
-               "<label style = 'font-weight:normal'>"+date + ", " +time +"</label>"+
+               "<div style = 'font-weight:normal'>"+date + ", " +time +"</div>"+
             "</div>"+
             "<div class='col-md-5'>"+
-                "<label style = 'font-weight:normal'>"+ val.EventName +"</label>"+
+                "<div style = 'font-weight:normal'>"+ val.EventName +"</div>"+
             "</div>"+
             "<div class='col-md-3'>"+
-                "<label style = 'font-weight:normal'>"+ val.DriverName +" "+val.DriverSurname +"</label>"+
+                "<div style = 'font-weight:normal'>"+ val.DriverName +" "+val.DriverSurname +"</div>"+
             "</div>"+
         "</div>"
+        
+        
     });
     console.log(val);
     item.appendTo("#agendaList");
