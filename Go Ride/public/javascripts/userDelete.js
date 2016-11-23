fce6892a9b38e5a9c0e36ccf265a95811246b51f
KinkyRobot <richardosmart@gmail.com>
2016-11-23 04:18:25 +0200

    userDelete

--- Go Ride/public/javascripts/userDelete.js
+++ Go Ride/public/javascripts/userDelete.js
@@ -1,1 +1,11 @@
 $("#headerText").html("Delete Account");
+
+$('#confirmDelete').click(function () {
+    // var passwordInput = $("#passwordInput").val();
+    // console.log(passwordInput);
+    $.post("/editUser", {
+        // password: passwordInput
+      }, function (success) {
+        console.log(true);
+      });
+  })
